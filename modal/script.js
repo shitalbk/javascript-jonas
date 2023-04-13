dacb82b625990f917d52d4f14b6b8d4d4dc0d074
Shital <shitalbk36@gmail.com>
2023-04-13 11:12:42 -0400

    Pig Game

--- modal/script.js
+++ modal/script.js
@@ -8,21 +8,21 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 const closeModal = function() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-}
+};
 const openModal = function() {
   console.log('Button clicked!!');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-}
+};
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e){
+document.addEventListener('keydown', function(e) {
   console.log(e.key);
-  if(e.key === 'Escape' && !modal.classList.contains('hidden')){
-      closeModal();
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
   }
 });
