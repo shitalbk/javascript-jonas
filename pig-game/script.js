21f00c7bc6bdf8015c32d20673c89c659c45f4ef
Shital <shitalbk36@gmail.com>
2023-04-13 14:11:48 -0400

    Pig Game

--- pig-game/script.js
+++ pig-game/script.js
@@ -17,34 +17,60 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0,0];
+const scores = [0, 0];
 let currentScore = 0;
+let playerScore = 0;
 let activePlayer = 0;
+let playerScre = 0;
 
-
-const buttonRoll = function(){
+const buttonRoll = function() {
   console.log('Button is clicked!!!');
   // 1. Generating a random dice roll
-   const dice = Math.trunc(Math.random() * 6) + 1;
-   console.log(dice);
-  
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
   // 2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
 
   // Check for roll 1: if true, switch to next player
-    if(dice !== 1){
-      // Add dice to current score
-      currentScore += dice;
-      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-      
-    }else{
-      // Switch to next player
-      document.getElementById(`current--${activePlayer}`).textContent = 0;
-      currentScore = 0;
-      activePlayer = activePlayer === 0 ? 1 : 0;  
-      player0El.classList.toggle('player--active');
-      player1El.classList.toggle('player--active');
-    }  
-}
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+    console.log("Current Score " +  currentScore);
+  } else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+};
+const buttonHold = function() {
+  console.log('Hold Button');
+  // current player value
+  
+    console.log("Current Score " +  currentScore);
+    console.log("Player Score " + playerScre);
+    currentScore += playerScre;
+    document.getElementById(`score--${activePlayer}`).textContent = currentScore;
+    playerScre = Number(document.getElementById(`score--${activePlayer}`).textContent);
+    console.log("Player Value is "+ playerScre);
+ 
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    // current player value set to zero
+        currentScore = 0;
+    // switch player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    console.log("Current Score " +  currentScore);
+  // score 0 value set
+};
+
 btnRoll.addEventListener('click', buttonRoll);
+btnHold.addEventListener('click', buttonHold);
