6247825fa2cd62cd85e5591f3688ef5fcca69313
Shital BK <shitalbk36@gmail.com>
2023-09-09 13:22:08 -0400

    Delete src directory

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '59972828-8768-4365-a72f-9450c4d84a2d';
-export const MODAL_CLOSE_SEC = 2.5;
